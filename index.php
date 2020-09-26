efc415bcfff7940df204eb5b184884d302c58747
Frank798034 <Frank798034@users.noreply.github.com>
2020-09-26 15:30:24 -0400

    changement dans le css et la page index.php du thème

--- index.php
+++ index.php
@@ -16,7 +16,20 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
-
+	<nav id="menu" title="Menu">
+        <a href="#">
+            <div></div>
+            <div></div>
+            <div></div>
+        </a>
+    </nav>
+    <nav class="logo">
+            <img src="media/logo.png" alt="Logo TIM" title="Techniques d'intégration multimédia - Collège de Maisonneuve">
+    </nav>
+    <nav class="choix">
+        <p id="gauche"><a href="cheminement.html" title="Cheminement"><b>Cheminement</b></a></p>
+        <p id="droite"><a href="info.html" title="Autres infos"><b>Autres infos</b></a></p>
+    </nav>
 		<?php
 		if ( have_posts() ) :
 
