04e431b79ecf6d487f56e314af79964755b00456
Frank798034 <Frank798034@users.noreply.github.com>
2020-09-26 15:33:38 -0400

    modification de d'index.php

--- index.php
+++ index.php
@@ -31,37 +31,37 @@ get_header();
         <p id="droite"><a href="info.html" title="Autres infos"><b>Autres infos</b></a></p>
     </nav>
 		<?php
-		if ( have_posts() ) :
+		// if ( have_posts() ) :
 
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+			// if ( is_home() && ! is_front_page() ) :
+				// ?>
+				<!-- <header> -->
+					<!-- <h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1> -->
 				</header>
 				<?php
-			endif;
+			// endif;
 
 			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+			// while ( have_posts() ) :
+				// the_post();
 
 				/*
 				 * Include the Post-Type-specific template for the content.
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
 				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+		// 		get_template_part( 'template-parts/content', get_post_type() );
 
-			endwhile;
+		// 	endwhile;
 
-			the_posts_navigation();
+		// 	the_posts_navigation();
 
-		else :
+		// else :
 
-			get_template_part( 'template-parts/content', 'none' );
+		// 	get_template_part( 'template-parts/content', 'none' );
 
-		endif;
-		?>
+		// endif;
+		// ?>
 
 	</main><!-- #main -->
 
