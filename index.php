529d9fcd7b41a4a04f07a9977aff35b6e2b374b9
Frank798034 <Frank798034@users.noreply.github.com>
2020-09-26 21:03:42 -0400

    ajout de l'arrière-plan

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ get_header();
         </a>
     </nav>
     <nav class="logo">
-            <img src="wp-content/uploads/images/logo.png" alt="Logo TIM" title="Techniques d'intégration multimédia - Collège de Maisonneuve">
+            <img src="wp-content/themes/homoioi1/images/logo.png" alt="Logo TIM" title="Techniques d'intégration multimédia - Collège de Maisonneuve">
     </nav>
     <nav class="choix">
         <p id="gauche"><a href="cheminement.html" title="Cheminement"><b>Cheminement</b></a></p>
