e6aae9ff37d30b0903a8ede26f9e6202eff1826c
Maxime Rioux <maxime.rioux7@gmail.com>
2020-10-21 17:22:57 -0400

    page de cheminement

--- sidebar.php
+++ sidebar.php
@@ -4,16 +4,14 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
- * @package Homoioi
+ * @package homoioi
  */
 
-// if ( ! is_active_sidebar( 'sidebar-1' ) ) {
-	// return;
-// }
+if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+	return;
+}
 ?>
 
-<!-- <aside id="secondary" class="widget-area"> -->
-	<?php
-	// dynamic_sidebar( 'sidebar-1' );
-	?>
-<!-- </aside>#secondary -->
+<aside id="secondary" class="widget-area">
+	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+</aside><!-- #secondary -->
