ebfdd1afd961319776d3c8288a011c8c404b1382
QcDiablo <74427389+QcDiablo@users.noreply.github.com>
2020-12-02 11:15:53 -0500

    Ajout du code de la galerie d'images en PHP

--- template-parts/content-vie-etudiante.php
+++ template-parts/content-vie-etudiante.php
@@ -32,9 +32,9 @@
 
 
 			<!-- Ajouter toutes les parties à la page de la vie étudiante -->
-			<!--<div id="ve-evenements">
-				<?php //include 'vie-etudiante/ve-vie-etudiante.php'; ?>
-			</div>-->
+			<div id="ve-evenements">
+				<?php include 'vie-etudiante/ve-vie-etudiante.php'; ?>
+			</div>
 			<div id="ve-evenements">
 				<?php include 'vie-etudiante/ve-evenements.php'; ?>
 			</div>
