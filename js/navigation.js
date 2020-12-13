27d49138e8a1442b1d97dc18c2c0f49d80903ca9
Nick Joseph <niickyymjj@gmail.com>
2020-12-13 16:06:27 -0500

    Click à l'extérieur pour fermer le menu - petit bug pour réouvrir le menu, mais s'ouvre quand même

--- js/navigation.js
+++ js/navigation.js
@@ -1,17 +1,24 @@
-//** 
+/** 
 ( function() {
 	document.addEventListener("click", function(){
 		let NavMenu = document.getElementById("menu-ul");
 		let BtnMenu = document.getElementById("btn-menu");
-
+		
 		window.onclick = function(event) {
-			if(event.target != NavMenu && event.target !=BtnMenu){
+			if(event.target == BtnMenu) {
+				console.log("Input unchecked");
+				NavMenu.style.transform = "none";
+			}
+			
+			else if(event.target != NavMenu && event.target != BtnMenu){
 				if (BtnMenu.checked == true) {
+					console.log("Input checked");
 					NavMenu.style.transform = "translate(-100%, 0)";
 					BtnMenu.checked = false;									
 				}
-			}			
+			}
+						
 		}	
-	});		
+	});
 }() );
 //*/
