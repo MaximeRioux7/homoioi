f0e5910f584a954163e5cb755158ba4544df272c
QcDiablo <74427389+QcDiablo@users.noreply.github.com>
2020-12-13 21:32:20 -0500

    Changement de logo pour l'accueil

--- js/parallax.js
+++ js/parallax.js
@@ -1,10 +1,16 @@
 (function(){
     document.addEventListener("DOMContentLoaded", function(){
+        let actif = true;
+
+        let query = window.matchMedia("(max-width: 960px)");
+        query.addListener(MediaQuery);
+        MediaQuery(query);
+
         // Sélecteur CSS pour récupérer l'élément HTML
         let selecteur = ".logo-site";// insert logo class or id here
         // "Multiplicateur" pour le déplacement de l'élément
-        let facteur1 = 0.04;
-        let facteur2 = 0.04;
+        let facteur1 = 0.03;
+        let facteur2 = 0.03;
         let diffX, diffY;
         let transform = `
             translateX(-50%) 
@@ -13,7 +19,7 @@
         `;
         let elements = document.querySelectorAll(selecteur);
         function offsetObjects(mouseEvent){
-            for(let i = 0; i < elements.length; i++){
+            if(actif){
                 diffX = window.innerWidth/2 - mouseEvent.pageX;
                 diffY = window.innerHeight/2 - mouseEvent.pageY;
                 transform = `
@@ -21,7 +27,14 @@
                     translateY(calc(${diffY * facteur2}px - 50%)) 
                     rotate(30deg)
                 `;
-                
+            } else{
+                transform = `
+                    translateX(-50%) 
+                    translateY(-50%) 
+                    rotate(30deg)
+                `;
+            }
+            for(let i = 0; i < elements.length; i++){
                 elements[i].style.transform = transform;
                 elements[i].style.msTransform = transform;
                 elements[i].style.webkitTransform = transform;
@@ -37,5 +50,9 @@
             document.addEventListener("mousemove", offsetObjects);
         }
         else console.warn(`Aucun élément répondant au sélecteur ${selecteur} n'a été trouvé.`);
+
+        function MediaQuery(query){
+            actif = !query.matches;
+        }
     }, false);
 })();
