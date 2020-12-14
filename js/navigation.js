cd265108a77f30b7e76ebac667eaf25e4204a74e
Nick Joseph <niickyymjj@gmail.com>
2020-12-13 20:39:06 -0500

    Bug réglé menu marche très bien

--- js/navigation.js
+++ js/navigation.js
@@ -1,23 +1,27 @@
 //** 
 ( function() {
-	document.addEventListener("click", function(){
+	document.addEventListener("DOMContentLoaded", function(){
 		let NavMenu = document.getElementById("menu-ul");
 		let BtnMenu = document.getElementById("btn-menu");
+		let Checked = false;
 		
 		window.onclick = function(event) {
-			if(event.target == BtnMenu) {
-				console.log("Input unchecked");
-				NavMenu.style.transform = "none";
-			}
 			
-			else if(event.target != NavMenu && event.target != BtnMenu){
-				if (BtnMenu.checked == true) {
-					console.log("Input checked");
+			if (event.target == BtnMenu && !Checked){
+				console.log("Input checked");
+				NavMenu.style.transform = "none";
+				Checked = true;
+			}			
+			
+						
+			else if((event.target != NavMenu || event.target == BtnMenu) && Checked){
+					console.log("Input unchecked");
 					NavMenu.style.transform = "translate(-100%, 0)";
 					BtnMenu.checked = false;									
-				}
+					Checked = false;
 			}						
-		}	
+		}
 	});
+	
 }() );
 //*/
