30ea8ed804d6bec21f5e288515817823a89f1c35
Nick Joseph <niickyymjj@gmail.com>
2020-12-13 20:41:57 -0500

    Menu Fonctionnel

--- js/navigation.js
+++ js/navigation.js
@@ -1,19 +1,19 @@
-//** 
+
 ( function() {
 	document.addEventListener("DOMContentLoaded", function(){
+		// Aller chercher les élements Menu et Menu Bouton
 		let NavMenu = document.getElementById("menu-ul");
 		let BtnMenu = document.getElementById("btn-menu");
 		let Checked = false;
 		
-		window.onclick = function(event) {
-			
+		// Lorsqu'on clique sur le bouton ou l'extérieur, le menu se ferme
+		window.onclick = function(event) {			
 			if (event.target == BtnMenu && !Checked){
 				console.log("Input checked");
 				NavMenu.style.transform = "none";
 				Checked = true;
 			}			
-			
-						
+									
 			else if((event.target != NavMenu || event.target == BtnMenu) && Checked){
 					console.log("Input unchecked");
 					NavMenu.style.transform = "translate(-100%, 0)";
@@ -22,6 +22,5 @@
 			}						
 		}
 	});
-	
 }() );
-//*/
+
